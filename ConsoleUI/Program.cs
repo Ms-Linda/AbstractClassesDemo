2e5b735c83385baf5a9fb527d20f07ce111c59af
Ms-Linda <wfhlinda@gmail.com>
2021-02-07 02:18:21 -0500

    Vehicle class and 3 properties

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -17,8 +17,8 @@ namespace ConsoleUI
             #region Vehicles
 
             /*
-             * Create an abstract class called Vehicle
-             * The vehicle class shall have three string properties Year, Make, and Model
+             * DONE - Create an abstract class called Vehicle
+             * DONE - The vehicle class shall have three string properties Year, Make, and Model
              * Set the defaults to something generic in the Vehicle class
              * Vehicle shall have an abstract method called DriveAbstract with no implementation
              * Vehicle shall have a virtual method called DriveVirtual with a base implementation.
