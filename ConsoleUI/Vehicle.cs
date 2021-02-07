332f53a4ed68d4bd97cf3022096232aecb8794c6
Ms-Linda <wfhlinda@gmail.com>
2021-02-07 02:36:06 -0500

    modified Vehicle properties added abstract method

--- ConsoleUI/Vehicle.cs
+++ ConsoleUI/Vehicle.cs
@@ -7,7 +7,15 @@ namespace ConsoleUI
     public abstract class Vehicle
     {
         public int Year { get; set; } = 1;
-        public string Make { get; set; } = "Default Make";
-        public string Model { get; set; } = "Default Model";
+        public string Make { get; set; } = "Generic Make";
+        public string Model { get; set; } = "Generic Model";
+
+        public abstract void DriveAbstract(); //"stubbed" out / Abstract method 
+                                              //doesn't have implentation / get set / scope
+                                              //there are parens with semicolon-this is how it's done
+                                              //for no implementation / empty parentheses and semicolon
+
+
+
     }
 }
